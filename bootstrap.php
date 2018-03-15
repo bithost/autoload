6b6f5c2e0fd7844bbd6bd6b67bc8d13c51d7aca0
Tadas <tadasa@gmail.com>
2018-03-15 20:36:02 +0200

    Loading both clases works. Need to make register function working.

--- bootstrap.php
+++ bootstrap.php
@@ -3,17 +3,44 @@
 class Psr4Autoloader
 {
     // add your code here...
-    static public function add($class) {
-        require_once __DIR__.'/src/ClassA.php';
-        require_once __DIR__.'/src/Subpackage/ClassB.php';
+    public function register()
+    {
+        spl_autoload_register(array($this, 'add'));
     }
     
-   // spl_autoload_register('Autoloader::add');
+     public function add($class) {
+        $prefix = 'Nfq\\Academy\\Homework\\';
+	
+	   $length = strlen($prefix);
+
+        $base_directory = __DIR__ . '/src/';
+
+       
+
+        //if(strncmp($prefix, $class, $length) !== 0) {
+       //     return;
+       //   }
+       $class_end = substr($class, $length);  
+
+        $file = $base_directory . str_replace('\\', '/', $class_end) . '.php';
+        
+       //require_once __DIR__.'/src/ClassA.php';
+       //  require_once __DIR__.'/src/Subpackage/ClassB.php';
+       if(file_exists($file)) {
+        require $file;
+      }
+       }
+    
+
+    
+
 }
 
-spl_autoload_register('Psr4Autoloader::add');
+
 
 $autoloader = new Psr4Autoloader();
-// $autoloader
-//      ->add('Nfq\\Academy\\Homework\\', __DIR__.'/src/')
-//      ->register();
+$autoloader
+   ->add('Nfq\\Academy\\Homework\\', __DIR__.'/src/')
+ //  ->add('Nfq\\Academy\\Homework\\', __DIR__.'/vendor/acme/');
+   ->register();
+    
