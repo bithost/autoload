29acd0e3c198dea49806bc28c8e7a7552b460b8f
Tadas <tadasa@gmail.com>
2018-03-15 20:35:31 +0200

    no change.

--- test.php
+++ test.php
@@ -5,7 +5,7 @@ use Nfq\Academy\Homework\Subpackage\ClassB;
 
 require_once __DIR__.'/bootstrap.php';
 
-require_once __DIR__.'/src/Subpackage/ClassB.php';
+
 
 $a = new ClassA();
 $a->doSomething();
